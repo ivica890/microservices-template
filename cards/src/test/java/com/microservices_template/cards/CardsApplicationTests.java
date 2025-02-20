1c2cc6e41b2e206bebb95d2e4a52bf45bdfa0824
icickovski <icickovski0@gmail.com>
2025-02-20 01:11:47 +0100

    initial setup

---
+++ cards/src/test/java/com/microservices_template/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_template.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
